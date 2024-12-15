654f18f274d2f74070d4d7cfb13b78c6ad5d9787
John Stanley Altonaga <74578077+twapegg@users.noreply.github.com>
2024-12-15 10:06:37 +0000

    feat: Remove loading spinner component and simplify Dashboard loading logic

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,25 +1,12 @@
-"use client";
-import React, { useState, useEffect } from "react";
 import Dashboard from "@/components/dashboard";
 import HeaderPage from "@/components/sub/headerpage";
-import LoadingSpinner from "@/components/ui/loading-spinner";
 
 const page = () => {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 2000); // Simulate loading time
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <div>
       <HeaderPage title="Dashboard" />
       <div className="py-8 px-6 lg:px-10">
-        {isLoading ? <LoadingSpinner /> : <Dashboard />}
+        <Dashboard />
       </div>
     </div>
   );
